eb4f519322afd50bcb861277ce4ba64b7cc14b90
MoutonSanglant <defresne.thierry@gmail.com>
2017-01-27 19:06:42 +0100

    half-merge

--- VM/src/main.c
+++ VM/src/main.c
@@ -6,7 +6,7 @@
 /*   By: tdefresn <tdefresn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/13 14:27:30 by tdefresn          #+#    #+#             */
-/*   Updated: 2017/01/27 19:01:35 by tdefresn         ###   ########.fr       */
+/*   Updated: 2017/01/27 19:05:48 by tdefresn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ t_corewar	g_corewar;
 
 int		main(int ac, char **av)
 {
-
 	ft_bzero(&g_corewar, sizeof(t_corewar));
 	if (ac < 2)
 		error(ERRNO_USAGE, NULL);
