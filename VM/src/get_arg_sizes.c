7b93ee7f5ebf01f83fb200b1c9b4a87f744a4292
MoutonSanglant <defresne.thierry@gmail.com>
2017-02-21 17:52:02 +0100

    fixed pc padding on bad opc for 2 args opcodes

--- VM/src/get_arg_sizes.c
+++ VM/src/get_arg_sizes.c
@@ -6,7 +6,7 @@
 /*   By: akopera <akopera@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/25 17:15:30 by akopera           #+#    #+#             */
-/*   Updated: 2017/02/20 17:59:18 by tdefresn         ###   ########.fr       */
+/*   Updated: 2017/02/21 17:49:13 by tdefresn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,10 @@ static void		get_type_of_arg(t_op_arg *arg, char octet_codage, int opc)
 }
 
 static size_t	get_argument_sizes(char octet_codage, int opcode,
-	t_op_arg args[3])
+											t_op_arg args[3], int args_count)
 {
+	int	i;
+
 	if (!g_op_tab[opcode].ocp)
 	{
 		if (g_op_tab[opcode].dir_short)
@@ -45,20 +47,23 @@ static size_t	get_argument_sizes(char octet_codage, int opcode,
 			args[0].size = DIR_SIZE;
 		return (1);
 	}
-	get_type_of_arg(&args[0], octet_codage >> 6 & 0b11, opcode);
-	get_type_of_arg(&args[1], octet_codage >> 4 & 0b11, opcode);
-	get_type_of_arg(&args[2], octet_codage >> 2 & 0b11, opcode);
+	i = 0;
+	while (i < args_count)
+	{
+		get_type_of_arg(&args[i], octet_codage >> (6 - (2 * i)) & 0b11, opcode);
+		i++;
+	}
 	return (2);
 }
 
-static void		get_arguments(char *pc, t_op_arg args[3])
+static void		get_arguments(char *pc, t_op_arg args[3], int args_count)
 {
-	int		offset;
 	int		i;
+	int		offset;
 
 	i = 0;
 	offset = 0;
-	while (i < 3)
+	while (i < args_count)
 	{
 		swap_endianess((char *)&args[i].value, (char *)&pc[offset],
 			args[i].size);
@@ -70,8 +75,12 @@ static void		get_arguments(char *pc, t_op_arg args[3])
 size_t			get_argument_op(t_proc *proc, int opcode, t_op_arg args[3])
 {
 	size_t	offset;
+	int		args_count;
 
-	offset = get_argument_sizes(proc->pc[1], opcode - 1, args);
-	get_arguments((char *)&proc->pc[offset], args);
+	args_count = 3;
+	if (opcode == 0x0d || opcode == 0x02 || opcode == 0x03)
+		args_count = 2;
+	offset = get_argument_sizes(proc->pc[1], opcode - 1, args, args_count);
+	get_arguments((char *)&proc->pc[offset], args, args_count);
 	return (offset + args[0].size + args[1].size + args[2].size);
 }
