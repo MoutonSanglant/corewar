5f9bbfef322d2a99895367025c9c08ef21822fe0
MoutonSanglant <defresne.thierry@gmail.com>
2017-02-27 19:08:25 +0100

    test: pre-read opc

--- VM/src/get_arg_sizes.c
+++ VM/src/get_arg_sizes.c
@@ -6,7 +6,7 @@
 /*   By: akopera <akopera@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/25 17:15:30 by akopera           #+#    #+#             */
-/*   Updated: 2017/02/27 18:58:55 by tdefresn         ###   ########.fr       */
+/*   Updated: 2017/02/27 19:07:14 by tdefresn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,8 +87,8 @@ size_t			get_argument_op(t_proc *proc, int opcode, t_op_arg args[3])
 	args_count = 3;
 	if (opcode == 0x0d || opcode == 0x02 || opcode == 0x03)
 		args_count = 2;
-	offset = get_argument_sizes(proc->pc[1], opcode - 1, args, args_count);
-	//offset = get_argument_sizes(proc->opc, opcode - 1, args, args_count);
+	//offset = get_argument_sizes(proc->pc[1], opcode - 1, args, args_count);
+	offset = get_argument_sizes(proc->opc, opcode - 1, args, args_count);
 	get_arguments((char *)&proc->pc[offset], args, args_count);
 	return (offset + args[0].size + args[1].size + args[2].size);
 }
