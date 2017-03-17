0e96b2c0acfcc2a824a5891a6c8ee895fc44a4f6
MoutonSanglant <defresne.thierry@gmail.com>
2017-03-17 17:09:53 +0100

    issue: bus error in live_op.c

--- VM/src/live_op.c
+++ VM/src/live_op.c
@@ -6,30 +6,26 @@
 /*   By: akopera <akopera@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/29 18:22:00 by akopera           #+#    #+#             */
-/*   Updated: 2017/03/14 22:27:29 by tdefresn         ###   ########.fr       */
+/*   Updated: 2017/03/17 17:03:30 by tdefresn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-static void	mark_byte(char *pc)
-{
-	int		offset;
-
-	offset = pc - g_corewar.cycle_infos.arena;
-	g_corewar.cycle_infos.byte_infos[offset].live = 50;
-}
 
 void		live_op(t_proc *proc, t_op_arg args[3])
 {
-	t_player	*player;
+	t_cycle_infos	*infos;
+	t_player		*player;
 
+	infos = &g_corewar.cycle_infos;
 	proc->live++;
 	if ((player = find_player(args[0].value)))
 	{
-		mark_byte(proc->pc);
-		g_corewar.cycle_infos.last_live = player->number;
-		player->last_live = g_corewar.cycle_infos.count;
+//		mark_byte(proc->pc);
+		infos->byte_infos[proc->pc - infos->arena].live = 50;
+		infos->last_live = player->number;
+		player->last_live = infos->count;
 		player->current_lives++;
 	}
 }
