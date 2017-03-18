f09813b96dfd03c8069d8f5bbcde6a9f3dd555e5
MoutonSanglant <defresne.thierry@gmail.com>
2017-03-18 13:36:23 +0100

    +updated check script -removed garbage file

--- VM/check.sh
+++ VM/check.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 clear
 
 function match
@@ -7,4 +9,15 @@ function match
 	diff <(./corewar -h $1) <(../resources/corewar $1)
 }
 
-match "-d $1 $2 $3 $4 $5"
+match "-d 0 $1 $2 $3 $4"
+match "-d 1 $1 $2 $3 $4"
+match "-d 5 $1 $2 $3 $4"
+match "-d 10 $1 $2 $3 $4"
+match "-d 25 $1 $2 $3 $4"
+match "-d 200 $1 $2 $3 $4"
+match "-d 1000 $1 $2 $3 $4"
+match "-d 2800 $1 $2 $3 $4"
+match "-d 7600 $1 $2 $3 $4"
+match "-d 13400 $1 $2 $3 $4"
+match "-d 24300 $1 $2 $3 $4"
+match "-d 36500 $1 $2 $3 $4"
